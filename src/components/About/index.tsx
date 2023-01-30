fa500b7431470ddfb42bbeedab689459ea54e24b
unknown <tai.tranhuu2002@gmail.com>
2023-01-30 19:55:00 +0700

    fix: responsive

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,19 +1,17 @@
-import { MdOutlineArrowRight } from 'react-icons/md';
-import { AnimationOnScroll } from 'react-animation-on-scroll';
-import { useTranslation } from 'react-i18next';
-import React from 'react';
+import { MdOutlineArrowRight } from "react-icons/md";
+import { AnimationOnScroll } from "react-animation-on-scroll";
+import { useTranslation } from "react-i18next";
+import React from "react";
 
-const About = React.forwardRef<HTMLDivElement>((props,ref) => {
-  const {t} = useTranslation();
-  
+const About = React.forwardRef<HTMLDivElement>((props, ref) => {
+  const { t } = useTranslation();
+
   return (
     <div ref={ref} id="about" className="container-ct">
       <div className="section-header">
         <AnimationOnScroll animateIn="animate__zoomIn">
-          <h2 className="section-title">{t('about')}</h2>
-          <p className="text-center paragraph px-5">
-            {t('michael_dell')}
-          </p>
+          <h2 className="section-title">{t("about")}</h2>
+          <p className="text-center paragraph px-5">{t("michael_dell")}</p>
         </AnimationOnScroll>
       </div>
       <div className="flex flex-col lg:flex-row gap-10">
@@ -30,34 +28,31 @@ const About = React.forwardRef<HTMLDivElement>((props,ref) => {
           <div className="flex-grow">
             <h3 className="section-sub-title">Web Developer ( Front-end ) </h3>
             <div className="grid grid-cols-2 gap-5 my-5">
-              <Info name={t('full_name')} value={'Trần Hữu Tài'}/>
-              <Info name={t('dob')} value={'23/02/2002'}/>
-              <Info name={t('phone_number')} value={'0931983495'}/>
-              <Info name={t('email')} value={'tai.tranhuu2002@gmail.com'}/>
-              <Info name={t('city')} value={'Thành phố Huế'}/>
-              <Info name={t('nation')} value={'Việt Nam'}/>
+              <Info name={t("full_name")} value={"Trần Hữu Tài"} />
+              <Info name={t("dob")} value={"23/02/2002"} />
+              <Info name={t("phone_number")} value={"0931983495"} />
+              <Info name={t("email")} value={"tai.tranhuu2002@gmail.com"} />
+              <Info name={t("city")} value={"Thành phố Huế"} />
+              <Info name={t("nation")} value={"Việt Nam"} />
             </div>
-            <p className="text-justify paragraph">
-              {t('about_description')}
-            </p>
+            <p className="text-justify paragraph">{t("about_description")}</p>
           </div>
         </AnimationOnScroll>
       </div>
     </div>
   );
-  
-})
+});
 
 type InfoType = {
   name: string;
   value: string;
 };
 
-const Info = ({name, value}: InfoType) => {
+const Info = ({ name, value }: InfoType) => {
   return (
     <div className="flex items-center flex-nowrap gap-1">
-      <MdOutlineArrowRight className="text-primary min-w-[15px] min-h-[15px]"/>
-      <p>
+      <MdOutlineArrowRight className="text-primary min-w-[15px] min-h-[15px]" />
+      <p className="break-all">
         <span className="font-bold text-color">{name}</span>: <span>{value}</span>
       </p>
     </div>
